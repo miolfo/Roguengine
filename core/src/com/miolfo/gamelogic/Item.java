4eb4fd78cde390aea28031cbebde3686d599d2c7
Mikko Forsman <mikko.o.forsman@gmail.com>
2016-07-06 19:08:43 +0300

    Added first version of Item class

---
+++ core/src/com/miolfo/gamelogic/Item.java
@@ -0,0 +1,30 @@
+package com.miolfo.gamelogic;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by Mikko Forsman on 6.7.2016.
+ */
+public class Item {
+    private String mName = "";
+    private String mDescription = "";
+    private Sprite mSprite;
+
+    public Item(String name, String description, int maxDurability, Sprite sprite){
+        this.mName = name;
+        this.mDescription = description;
+        this.mSprite = sprite;
+    }
+
+    public String GetName(){
+        return mName;
+    }
+
+    public String GetDescription(){
+        return mDescription;
+    }
+
+    public Sprite GetSprite(){
+        return mSprite;
+    }
+}
